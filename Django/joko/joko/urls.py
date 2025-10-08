bf1ce59b7c15c732e23263d8dfa7584a77256ded
Tomas Matis <tomasbelaxinen@gmail.com>
2025-10-08 10:08:52 +0200

    -kalkulacka

--- Django/joko/joko/urls.py
+++ Django/joko/joko/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('jokoApp/', include('jokoApp.urls')),
+    path('kalkulacka/', include('kalkulacka.urls')),
 ]
---
+++ Django/joko/kalkulacka/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+        if request.method == "GET":
+                vysledok = 0
+        if request.method == "POST":
+                a = int(request.POST['a'])
+                b = int(request.POST['b'])
+                operator = request.POST['operator']
+                if operator == "+":
+                        vysledok = a+b
+                elif operator == "-":
+                        vysledok = a-b
+                elif operator == "*":
+                        vysledok = a*b
+                elif operator == "/":
+                        vysledok = a/b
+                else: vysledok = 0
+        return render(request, 'kalkulacka/index.html', dict(vysledok=vysledok))
