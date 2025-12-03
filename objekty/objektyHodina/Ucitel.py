e203bcb74b9ef543629ae9dd6a350ba1c8bb1a3e
Tomas Matis <tomasbelaxinen@gmail.com>
2025-12-03 09:56:20 +0100

    -Ucitelia, null class

--- objekty/objektyHodina/Ucitel.py
+++ objekty/objektyHodina/Ucitel.py
@@ -9,7 +9,13 @@ class Ucitel(Person):
         self.classs = classs
 
     def greet(self):
-        print(f"Ahojky ja som {self.title}{self.name} {self.surName}, ucim {self.subject} a som triedny ucitel triedy {self.classs}")
+        if self.classs != None:
+            print(f"Ahojky ja som {self.title}{self.name} {self.surName}, ucim {self.subject} a som triedny ucitel triedy {self.classs}.")
+        else:
+            print(f"Ahojky ja som {self.title}{self.name} {self.surName}, ucim {self.subject}.")
 
     def __str__(self):
-        return(f"{self.title} {self.name} {self.surName} {(datetime.date.today().year - int(self.year))} - Ucitel, Predmet: {self.subject}, Trieda: {self.classs}")
+        if self.classs != None:
+            return(f"{self.title} {self.name} {self.surName} {(datetime.date.today().year - int(self.year))} - Ucitel, Predmet: {self.subject}, Trieda: {self.classs}")
+        else: 
+            return(f"{self.title} {self.name} {self.surName} {(datetime.date.today().year - int(self.year))} - Ucitel, Predmet: {self.subject}")
